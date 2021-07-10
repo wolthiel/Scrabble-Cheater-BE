3143943a819197a49ae94900ed3d7e4f24b407f1
wolthiel <wolfgang.thiele@student.htw-berlin.de>
2021-07-10 13:19:07 +0200

    uploadWolfie

--- src/Scrabble/HashTable.java
+++ src/Scrabble/HashTable.java
@@ -8,7 +8,32 @@ public class HashTable {
 		Hashtable<String, LinkedList> hm = new Hashtable<String, LinkedList>();
 	}
 	
-	public void put(String key, Object object) {
-		
+	public void put(String key, String object) {
+		if (hm.containsKey(key)) {
+			searchList(key, object);
+		}
+		else {
+			createEntry(key,object);
+		}
+	}
+	
+	public String[] get(String key) {
+		LinkedList list = hm.get(key);
+		String[] returnArray = (String[]) list.toArray();
+		return returnArray;
+	}
+	
+	private void searchList(String key, String object) {
+		LinkedList list = hm.get(key);
+		if (!list.contains(object)) {
+			list.addLast(object);
+		}
+		hm.put(key,list);
+	}
+	
+	private void createEntry(String key, String object) {
+		LinkedList list = new LinkedList();
+		list.add(object);
+		hm.put(key, list);
 	}
 }
