6ef2a4a2360426494aa6f9891f09633cdc4d5629
wolthiel <wolfgang.thiele@student.htw-berlin.de>
2021-07-17 21:33:57 +0200

    Update ScrabbleCheater.java

--- src/Scrabble/ScrabbleCheater.java
+++ src/Scrabble/ScrabbleCheater.java
@@ -12,6 +12,8 @@ public class ScrabbleCheater {
 		}
 		
 		System.out.println(Arrays.toString( dictionary.lookup("Actress")));
+		System.out.println("The longest chain is "+ Arrays.toString( dictionary.longestChain()));
+		System.out.println("It's length is "+ dictionary.longestChain().length );
 //		System.out.println(dictionary.getPermutation("adeelrt"));
 //		System.out.println(dictionary.longestChain());
 //		System.out.println(dictionary.longestChain().length);
