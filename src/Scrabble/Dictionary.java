ef6214d48188ad50aea55e85a45ae126b95560f2
ruthWenzel <Ruth.Wenzel@Student.HTW-Berlin.de>
2021-07-17 20:40:47 +0200

    changes

--- src/Scrabble/Dictionary.java
+++ src/Scrabble/Dictionary.java
@@ -18,17 +18,6 @@ public class Dictionary {
 	Scanner myReader = new Scanner(f);
 	while(myReader.hasNextLine())
 		{
-		String data = myReader.nextLine().toLowerCase();		
-		wordsCounter += 1;		
-		System.out.println(data);		
-		}
-	}
-	
-	public Dictionary() throws FileNotFoundException {
-	f = new File("");
-	Scanner myReader = new Scanner(f);
-	while(myReader.hasNextLine())
-		{
 		String data = myReader.nextLine();
 		wordsCounter += 1;
 		System.out.println(data);
@@ -49,31 +38,13 @@ public class Dictionary {
 	public void put (String input) {
 		
 	}
-	
-	// hash function
-		public static int getHashValue (String key) {
-			//wenn die Normalisierungsmethode bereits geschrieben ist, 
-			// kann diese gelöscht werden
-			char[] letters = key.toUpperCase().toCharArray();
-			Arrays.sort(letters);
-			
-			
-			// converts key in int -> normalizes the value of the chars
-			int hashValue = 0;
-			for (int i =0; i < letters.length; i++) {
-				// 31 is prime number -> https://horstmann.com/htw/2021/info2/day17/#(11)
-				hashValue = 31 * hashValue + letters[i];
-			}
-			
-			return hashValue;
-		}
 		
 	public static int getHashValue (String key) {
 		//wenn die Normalisierungsmethode bereits geschrieben ist, 
 		// kann diese gelöscht werden
 		char[] letters = key.toUpperCase().toCharArray();
 		Arrays.sort(letters);
-		
+		System.out.println(letters);
 		
 		// converts key in int -> normalizes the value of the chars
 		int hashValue = 0;
